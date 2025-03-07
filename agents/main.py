983e06e42c3c41b76615f32078c570721ec747e5
VivekMara <vivek.v.mara@gmail.com>
2025-03-07 18:52:50 +0000

    fixed some bugs

--- agents/main.py
+++ agents/main.py
@@ -1,5 +1,5 @@
 import httpx
-from tools import add_memory, tool_calling, get_current_weather, ollama_chat_post_req, ollama_generate_post_req
+from tools import add_memory_for_chat, tool_calling, get_current_weather, ollama_chat_post_req, ollama_generate_post_req
 
 
 
@@ -8,21 +8,27 @@ client = httpx.Client()
 
 
 
-
+#for demonstrating tool calling
 while True:
     inp = input("USER: ")
     if inp == "q":
         print("GOOD BYE")
         break
-    data_list = tool_calling(inp)
-    if data_list[2] == None:
-        add_memory(user_input=inp, ollama_client=client)
-    if data_list[1] == "get_current_weather":
-        resp = get_current_weather(data_list[2])
-        payload_data_with_weather = {
+    data_list = tool_calling(user_input=inp, ollama_client=client)
+    resp = get_current_weather(data_list[2])
+    payload_data_with_weather = {
                 "model" : "llama3.2:3b",
-                "prompt" : f"You are an AI agent who has already used the tools to get the weather details at the place {inp} which has the weather forecast of {resp} celsius, your task is to tell this data to the user casually.",
+                "prompt" : f"You are an AI agent who has already used the tools to get the weather details at the place {data_list[2]} which has the weather forecast of {resp} celsius, your task is to tell this data to the user casually.",
                 "stream" : False
             }
-        llm_resp = ollama_generate_post_req(ollama_client=client, payload=payload_data_with_weather)
-        print(llm_resp["response"])
+    llm_resp = ollama_generate_post_req(ollama_client=client, payload=payload_data_with_weather)
+    print(llm_resp["response"])
+
+#for demonstrating chatting with memory
+# while True:
+#     inp = input("USER: ")
+#     if inp == "q":
+#         print("GOOD BYE")
+#         break
+#     resp = add_memory_for_chat(user_input=inp, ollama_client=client)
+#     print(resp["content"])
--- agents/tools.py
+++ agents/tools.py
@@ -14,7 +14,33 @@ def ollama_generate_post_req(ollama_client, payload):
     return response.json()
 
 
-def add_memory(user_input : str, ollama_client : object) -> str:
+def add_memory_for_chat(user_input : str, ollama_client : object) -> str:
+    data = {
+        "role" : "user",
+        "content" : f"{user_input}"
+    }
+    with open("data.json", "r") as file:
+        json_data = json.load(file)
+        json_data["messages"].append(data)
+    with open("data.json", "w") as file:
+        json.dump(json_data, file, indent=4)
+        
+
+    with open("data.json", "r") as file:
+        loaded_data = json.load(file)
+        response = ollama_chat_post_req(ollama_client=ollama_client, payload=loaded_data)
+        new_chat = response["message"]
+    
+
+    with open("data.json", "r") as file:
+        new_data = json.load(file)
+        new_data["messages"].append(new_chat)
+    with open("data.json", "w") as file:
+        json.dump(new_data, file, indent=4)
+
+    return new_chat
+
+def add_memory_for_chat_with_tools(user_input : str, ollama_client : object) -> str:
     data = {
         "role" : "user",
         "content" : f"{user_input}"
@@ -55,7 +81,7 @@ def tool_calling(user_input : str, ollama_client : object) -> List:
                 "type" : "function",
                 "function" : {
                     "name" : "get_current_weather",
-                    "description" : "Get the current weather for a given location only if the user explicitly specifies that he wants to know the weather of a certain location otherwise please carry a normal conversation with the user",
+                    "description" : "Get the current weather for a given location only if the user explicitly specifies that he or she wants to know the weather of a certain location otherwise please reply according to the user's request.",
                     "parameters" : {
                         "type" : "object",
                         "parameters" : {
@@ -82,7 +108,7 @@ def tool_calling(user_input : str, ollama_client : object) -> List:
 
 def get_current_weather(city):
     if city == "Bengaluru":
-        return "10°"
+        return "-1°"
     if city == "Mysore":
         return "20°"
     else:
