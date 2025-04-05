e6ae333b83b9240cfe83f88d82d4a1c3cf4ae0ff
VivekMara <vivek.v.mara@gmail.com>
2025-04-05 14:31:08 +0000

    implemented all task ops

--- primary_backend/cmd/handlers/db.go
+++ primary_backend/cmd/handlers/db.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 
-	_ "github.com/lib/pq"
 	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
+
 
 type Task struct {
 	ID uuid.UUID `json:"id"`
@@ -14,11 +15,14 @@ type Task struct {
 	STATUS bool `json:"status"`
 }
 
+
 func DB_connect() (*sql.DB , error) {
 	connStr := "postgres://darthman:darthman05@localhost:5432/go_backend_db?sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	return db,err
 }
+
+
 func Create_table(db *sql.DB) error {
 	_, err := db.Exec(`
 	CREATE TABLE IF NOT EXISTS tasks (
@@ -29,34 +33,58 @@ func Create_table(db *sql.DB) error {
 
 	return err
 }
-func Create_task(db *sql.DB,name string) (Task,error) {
-	defer db.Close()
 
+
+func Create_task(db *sql.DB, name string) (Task, error) {
 	var task = Task{
 		ID: uuid.New(),
 		NAME: name,
 		STATUS: false,
 	}
-
-
 	_, err := db.Exec(`
 	INSERT INTO tasks (id, name, status) VALUES ($1, $2, $3)`, task.ID, task.NAME, task.STATUS)
 
+	defer db.Close()
 	return task,err
 }
 
-func Read_task(){
-	
+
+func Read_task(db *sql.DB, name string) (Task, error) {
+	row := db.QueryRow("SELECT * FROM tasks WHERE name = $1", name)
+	var task Task
+	err := row.Scan(&task.ID, &task.NAME, &task.STATUS)
+	return task, err
 }
 
-func Read_tasks(){
-	fmt.Println("Read tasks")
+
+func Read_tasks(db *sql.DB) ([]Task, error) {
+	rows, err := db.Query("SELECT * FROM tasks")
+	defer rows.Close()
+	tasks := make([]Task, 0)
+	for rows.Next(){
+		var task Task
+		err = rows.Scan(&task.ID, &task.NAME, &task.STATUS)
+		if err != nil {
+			log.Fatal("Scan error: ", err)
+			continue
+		}
+		tasks = append(tasks, task)
+
+	}
+	return tasks, err
 }
 
-func Update_task(){
-	fmt.Println("Update a task")
+
+func Update_task(db *sql.DB, task Task) error {
+	_, err := db.Exec("UPDATE tasks SET name = $1, status = $2 WHERE id = $3", task.NAME, task.STATUS, task.ID)
+
+	return err
 }
 
-func Delete_task(){
-	fmt.Println("Delete a task")
+
+func Delete_task(db *sql.DB, id uuid.UUID)(int64, error, error){
+	res, err1 := db.Exec("DELETE FROM tasks WHERE id = $1", id)
+	affectedRows, err2 := res.RowsAffected()
+
+	return affectedRows, err1, err2
 }
--- primary_backend/cmd/main.go
+++ primary_backend/cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"primary_backend/cmd/handlers"
+
+	"github.com/google/uuid"
 )
 
 func main(){
+	//connect db
 	db, err := handlers.DB_connect()
 	if err != nil {
 		log.Fatal("Error connecting to db")
@@ -14,7 +17,7 @@ func main(){
 		fmt.Println("Connected to db")
 	}
 
-
+	//create table
 	err = handlers.Create_table(db)
 	if err != nil {
 		log.Fatal("Error creating a table")
@@ -22,7 +25,7 @@ func main(){
 		fmt.Println("Created the table")
 	}
 
-	
+	//create a task
 	resp,err := handlers.Create_task(db, "second task")
 	if err != nil {
 		log.Fatal("Error inserting data:", err)
@@ -30,4 +33,54 @@ func main(){
 		fmt.Println("Task inserted successfully")
 	}
 	fmt.Println(resp)
+
+	//retrieve a task from db using it's name
+	task, err := handlers.Read_task(db, "first task")
+	if err != nil {
+		log.Fatal("Error retrieving data:", err)
+	} else {
+		fmt.Println("Task retrieved successfully")
+	}
+	fmt.Println(task)
+
+	//retrieve all the tasks
+	tasks, err := handlers.Read_tasks(db)
+	if err != nil {
+		log.Fatal("Error retrieving data:", err)
+	} else {
+		fmt.Println("Tasks retrieved successfully")
+	}
+	fmt.Println(tasks)
+
+	//update a task with new values
+	UpdatedTask := handlers.Task{
+		ID: uuid.MustParse("c062c7e9-9c01-488a-9830-3f998ada1d56"),
+		NAME: "first task",
+		STATUS: true,
+	}
+	err = handlers.Update_task(db, UpdatedTask)
+	if err != nil {
+		log.Fatal("Error updating task", err)
+	} else {
+		fmt.Println("Successfully updated the task")
+	}
+
+	//delete a task using it's id
+	id := uuid.MustParse("c062c7e9-9c01-488a-9830-3f998ada1d56")
+	affectedRows, err1, err2 := handlers.Delete_task(db, id)
+	if affectedRows == 0 {
+		fmt.Println("No task found with the id ", id)
+	} else {
+		fmt.Println("Task deleted successfully")
+	}
+	if err1 != nil {
+		log.Fatal("Error deleting the task", err)
+	} else {
+		fmt.Println("Successfully deleted the task")
+	}
+	if err2 != nil {
+		log.Fatal("Error fetching the affected rows", err)
+	} else {
+		fmt.Println("Successfully fetched the updated rows")
+	}
 }
