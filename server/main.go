3ecf855ee5edd78bd789e73531a172f432c12aa0
VivekMara <vivek.v.mara@gmail.com>
2025-05-21 12:42:46 +0000

    feature: grpc

--- server/main.go
+++ server/main.go
@@ -1,2 +1,30 @@
-package server
+package main
 
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "server/server/greeterpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.SayHello(ctx, &pb.HelloRequest{Name: "Go Client"})
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+
+	log.Printf("Response from server: %s", res.Message)
+}
